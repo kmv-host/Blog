59e9fc0430953fa87c115f56c038668c63c50cc3
kmv-host@mail.ru <kmv-host@mail.ru>
2025-12-01 17:42:42 +0300

    В SD.cs Добавил  public static class SD  {      public static List<Post> posts = new(); // Список для хранения постов
         public static void AddPost(Post newPost)
         {
             posts.Add(newPost);
         }
     }

--- SD.cs
+++ SD.cs
@@ -1,10 +1,14 @@
-﻿namespace Blog
+﻿using Blog.Models.Domain;
+
+namespace Blog
 {
     public static class SD
     {
-        public static void AddPost()
-        {
+        public static List<Post> posts = new(); // Список для хранения постов
 
+        public static void AddPost(Post newPost)
+        {
+            posts.Add(newPost);
         }
     }
 }
