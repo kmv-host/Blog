c618df8fb433371554e4a43bd066b49d5b6afd46
kmv-host@mail.ru <kmv-host@mail.ru>
2025-12-01 17:36:46 +0300

    Добавил тестовую страницу на блог по ссылке http://localhost:5138/Home/Primer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Blog.Controllers
             return View();
         }
 
+        public IActionResult Primer()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
