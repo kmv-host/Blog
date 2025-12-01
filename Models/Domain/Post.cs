6dc7e40c39b63c2dff1c1edf8760de1635715e65
kmv-host@mail.ru <kmv-host@mail.ru>
2025-12-01 17:30:57 +0300

    В классе Post.cs  добавил  public Guid Id { get; set; }    // Уникальный идентификатор поста
     public string Name { get; set; }   // Заголовок поста
    
     public string Description { get; set; }   // Краткое описание поста
    
     public string Author { get; set; }   // Автор поста
    
     public DateTime Created { get; set; }   // Дата и время создания поста
    
     public DateTime LastUpdated { get; set; }   // Дата и время последнего обновления поста
    
     public bool IsVisible { get; set; }   // Флаг видимости поста

--- Models/Domain/Post.cs
+++ Models/Domain/Post.cs
@@ -2,5 +2,19 @@
 {
     public class Post
     {
+        public Guid Id { get; set; }    // Уникальный идентификатор поста
+
+        public string Name { get; set; }   // Заголовок поста
+
+        public string Description { get; set; }   // Краткое описание поста
+
+        public string Author { get; set; }   // Автор поста 
+
+        public DateTime Created { get; set; }   // Дата и время создания поста
+
+        public DateTime LastUpdated { get; set; }   // Дата и время последнего обновления поста
+
+        public bool IsVisible { get; set; }   // Флаг видимости поста
     }
+
 }
