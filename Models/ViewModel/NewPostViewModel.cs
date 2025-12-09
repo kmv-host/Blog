28b165c0f7237254b619300440aa121f288beea7
kmv-host@mail.ru <kmv-host@mail.ru>
2025-12-09 22:28:48 +0300

    Добавил в NewPostViewModel.cs  public string Name {  get; set; }
     public string Description { get; set; }
    
     public string ShortDescription { get; set; }
    
     public string Author { get; set; }
    
     public bool IsVisible { get; set; }

--- Models/ViewModel/NewPostViewModel.cs
+++ Models/ViewModel/NewPostViewModel.cs
@@ -2,5 +2,14 @@
 {
     public class NewPostViewModel
     {
+        public string Name {  get; set; }
+
+        public string Description { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string Author { get; set; }
+
+        public bool IsVisible { get; set; }
     }
 }
